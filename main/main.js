93ee50e1be3c1257b5225731bf70d72089c727e2
Christine233 <1040551136@qq.com>
2017-07-14 13:34:13 +0800

    first commit

--- main/main.js
+++ main/main.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function fizzBuzzWith(num) {
-  if(num.toString()){
-    return 'fizz';
-  }
+    function special(num) {
+      let numInfo = [];
+      let flag = 0;
+      let stringNum = num.toString();
+        for(let i in stringNum){
+          if(i === '3' && num % 3 === 0 && num % 5 != 0 && num % 7 != 0 ){
+            flag = -1;
+            break;
+          }
+        }
+        numInfo.push({number:num,})
+    }
 }
